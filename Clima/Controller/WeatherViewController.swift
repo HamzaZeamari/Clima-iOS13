a1003230908a8984198667ecd42e58901f3b87ee
HamzaZeamari <zeam.hamza@gmail.com>
2022-07-03 10:22:43 +0200

    Separate ViewController file using extensions
    Extension WeatherManagerDelegate
    Extension UITextFieldDelegate

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,31 +8,23 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
-
-
-
+class WeatherViewController: UIViewController {
 
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     var weatherManager = WeatherManager()
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         searchTextField.delegate = self
         weatherManager.delegate = self
-        // create the alert
-        let alert = UIAlertController(title: "My Title", message: "This is my message.", preferredStyle: .alert)
-
-        // add an action (button)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-
-        // show the alert
-        self.present(alert, animated: true, completion: nil)
     }
+}
+
+// MARK : - UITextFieldDelegate
+extension WeatherViewController : UITextFieldDelegate{
     @IBAction func searchPressed(_ sender: UIButton) {
         if(searchTextField.text != ""){
             print(searchTextField.text!)
@@ -40,7 +32,7 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
             searchTextField.placeholder = "Search"
         }
     }
-    
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool
     {
         searchTextField.endEditing(true)
@@ -55,15 +47,19 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
             return false
         }
     }
-    
+
     func textFieldDidEndEditing(_ textField: UITextField) {
         if let city = searchTextField.text{
             weatherManager.fetchWeather(cityName: city)
         }
-        
+
         searchTextField.text = ""
     }
+}
 
+// MARK : - WeatherManagerDelegate
+
+extension WeatherViewController : WeatherManagerDelegate{
     func didUpdateWeather(_ weatherManager: WeatherManager,weather: WeatherModel) {
         DispatchQueue.main.async{
             self.conditionImageView.image = UIImage(systemName: weather.conditionName)
